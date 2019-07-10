b1a471d02609017454ddea99a48fc0c032d96828
DetegiCE <martinok1103@naver.com>
2019-07-10 13:57:07 +0900

    Create read_data2.py

---
+++ Ch5/read_data2.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_file_name = 'linear_random.txt'
+xy=np.genfromtxt(data_file_name,dtype='float32')
+
+x_data = xy[:,0]
+y_data = xy[:,1]
+
+plt.plot(x_data,y_data,'bo',alpha=0.3)
+plt.show()
